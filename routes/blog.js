9115def05a7de12c45f5e8bcefd4fdee7e0854c4
shahidnabi-exe <shahidnabi041@gmail.com>
2025-08-29 17:09:15 +0500

    almost there

--- routes/blog.js
+++ routes/blog.js
@@ -27,10 +27,13 @@ router.get('/add-new', (req,res)=> {
 
 router.get('/:id', async(req, res) => {
     const blog = await Blog.findById(req.params.id).populate('createdBy');
+    const comments = await Comment.find({ blogId: req.params.id }).populate('createdBy');
     // console.log('blog', blog)
+    console.log('comments', comments)
     return res.render('blog', {
         user: req.user,
         blog,
+        comments,
     })
 })
 

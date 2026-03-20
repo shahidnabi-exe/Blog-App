3a0cd7910f3b043eced0e27cb636c91931a65197
Shahid Nabi <shahidnabi041@gmail.com>
2026-03-20 23:52:09 +0500

    responsive navbar

--- Client/src/Components/Layout/Navbar.jsx
+++ Client/src/Components/Layout/Navbar.jsx
@@ -1,33 +1,40 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import { UserData } from "../../Context/AuthContext";
 import './navbar.css';
 
 export default function Navbar() {
   const { user, logout } = UserData();
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const closeMenu = () => setMenuOpen(false);
 
   return (
     <nav className="navbar">
-      <Link to="/" className="navbar-brand">Blogify</Link>
+      <Link to="/" className="navbar-brand" onClick={closeMenu}>Blogify</Link>
 
-      <div className="navbar-links">
-        <Link to="/">Home</Link>
+      {/* Hamburger button */}
+      <button className="navbar-hamburger" onClick={() => setMenuOpen(prev => !prev)}>
+        <span></span>
+        <span></span>
+        <span></span>
+      </button>
+
+      {/* Links */}
+      <div className={`navbar-links ${menuOpen ? 'open' : ''}`}>
+        <Link to="/" onClick={closeMenu}>Home</Link>
 
         {user ? (
           <>
-            <Link to="/blog/new">Create Blog</Link>
-            <Link to="/my-blogs">My Blogs</Link>
-
-            <div className="navbar-user">
-              <div className="nav-avatar">
-                {user.name[0]}
-              </div>
-              <button onClick={logout} className="nav-btn-logout">Logout</button>
-            </div>
+            <Link to="/blog/new" onClick={closeMenu}>Create Blog</Link>
+            <Link to="/my-blogs" onClick={closeMenu}>My Blogs</Link>
+            <div className="nav-avatar">{user.name[0]}</div>
+            <button onClick={() => { logout(); closeMenu(); }} className="nav-btn-logout">Logout</button>
           </>
         ) : (
           <>
-            <Link to="/signup">Create Account</Link>
-            <Link to="/signin">Sign In</Link>
+            <Link to="/signup" onClick={closeMenu}>Create Account</Link>
+            <Link to="/signin" onClick={closeMenu}>Sign In</Link>
           </>
         )}
       </div>
